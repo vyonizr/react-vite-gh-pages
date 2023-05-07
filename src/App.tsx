31868b96f531076aeab84b4fbd2882631bc65b3f
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-05-07 13:16:03 +0700

    convert routing to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,46 @@
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.DEV)
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/',
-        element: (
-          <div>
-            <h1>Hello World</h1>
-            <Link to='about'>About Us</Link>
-          </div>
-        ),
-      },
-      {
-        path: 'about',
-        element: <div>About</div>,
-      },
-    ],
-    { basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/' }
-  )
+  // const router = createBrowserRouter(
+  //   [
+  //     {
+  //       path: '/',
+  //       element: (
+  //         <div>
+  //           <h1>Hello World</h1>
+  //           <Link to='about'>About Us</Link>
+  //         </div>
+  //       ),
+  //     },
+  //     {
+  //       path: 'about',
+  //       element: <div>About</div>,
+  //     },
+  //   ],
+  //   { basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/' }
+  // )
 
-  return <RouterProvider router={router} />
+  // return <RouterProvider router={router} />
+  return (
+    <BrowserRouter
+      basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}
+    >
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <h1>Hello World</h1>
+              <Link to='about'>About Us</Link>
+            </div>
+          }
+        />
+        <Route path='/about' element={<div>About</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
