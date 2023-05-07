a9db376b2c6445fd5d56361470f714ac884ca49f
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-05-07 07:44:37 +0700

    configure route basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: (
-        <div>
-          <h1>Hello World</h1>
-          <Link to='about'>About Us</Link>
-        </div>
-      ),
-    },
-    {
-      path: 'about',
-      element: <div>About</div>,
-    },
-  ])
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: (
+          <div>
+            <h1>Hello World</h1>
+            <Link to='about'>About Us</Link>
+          </div>
+        ),
+      },
+      {
+        path: 'about',
+        element: <div>About</div>,
+      },
+    ],
+    { basename: '/react-vite-gh-pages/' }
+  )
 
   return <RouterProvider router={router} />
 }
